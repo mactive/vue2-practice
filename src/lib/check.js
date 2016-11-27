de4a500c474c18eb306384fa1d6ef6a33a8dfbb8
mactive <mactive@gmail.com>
2016-11-27 21:13:47 +0800

    try es6 default export and other

---
+++ src/lib/check.js
@@ -0,0 +1,10 @@
+var valueA = "value_a";
+var value_b = "value_b";
+
+export default function check (name) {
+  console.log('default func', name)
+};
+export {
+  valueA,
+  value_b
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import defalutExport, {value_a, value_b} from './lib/check'
+
+defalutExport("haha");
+console.log(value_a);
+console.log(value_b);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
-})
+});
