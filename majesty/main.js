a0605c66bae12fc3d4050342769284290929b756
mactive <mactive@gmail.com>
2017-02-07 10:13:55 +0800

    change

--- majesty/main.js
+++ majesty/main.js
@@ -1,21 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
-import MajestyApp from './majesty-app'
+import TodoMvc from './todomvc/App.vue'
+import store from './store'
 import CustomFilters from './majesty/custom-filters'
 import ComputedProp from './majesty/computed-prop'
 import EmitListen from './majesty/emit-listen'
 Vue.use(VueRouter)
 Vue.use(Vuex)
 
-// new Vue({
-//   el: '#app',
-//   template: '<MajestyApp/>',
-//   components: { MajestyApp }
-// })
-
 const routes = [
-  { path: '/', component: MajestyApp },
+  { path: '/', component: TodoMvc },
   { path: '/filters', component: CustomFilters },
   { path: '/props', component: ComputedProp },
   { path: '/emitlisten', component: EmitListen}
@@ -25,16 +20,16 @@ const router = new VueRouter({
   routes: routes // （缩写）相当于 routes: routes
 })
 
-const store = new Vuex.Store({
-  state: {
-    votes: 10
-  },
-  mutations: {
-    upVote (state) {
-      state.votes++
-    }
-  }
-})
+// const store = new Vuex.Store({
+//   state: {
+//     votes: 10
+//   },
+//   mutations: {
+//     upVote (state) {
+//       state.votes++
+//     }
+//   }
+// })
 
 // 4. 创建和挂载根实例。
 // 记得要通过 router 配置参数注入路由，
@@ -43,4 +38,11 @@ const app = new Vue({
   router,
   store
 }).$mount('#app')
+
+// const app = new Vue({
+//   el: '#app',
+//   store,
+//   template: '<TodoMvc/>',
+//   components: { TodoMVC }
+// })
 
